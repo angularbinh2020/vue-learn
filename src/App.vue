593cff53f19c44f4641c6794f0550a6f0304ae1c
ITBU\binhdd3 <doducbinh1995@gmail.com>
2024-12-16 17:31:08 +0700

    init project

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+const elementId = ref(crypto.randomUUID())
+const htmlContent = ref('<p>Hello world</p>');
+const changeContent = () => {
+  htmlContent.value = `<p>Value: ${crypto.randomUUID()}</p>`
+}
+const changeId = () => {
+  elementId.value = crypto.randomUUID()
+}
+</script>
+
+<template>
+  <div v-html="htmlContent" v-bind:id="elementId"></div>
+  <button @click="changeContent">Thay đổi content</button>
+  <button @click="changeId">Thay đổi id</button>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
